2b36aff702a41dffee706316a096f5b22a4321b8
turkanamammadova <60044927+turkanamammadova@users.noreply.github.com>
2021-01-27 20:28:22 -0600

    Update simple.sh

--- simple.sh
+++ simple.sh
@@ -3,3 +3,5 @@
 echo "This is a very simple script for testing Jenkins - v2"
 
 echo "This is a very simple script for testing Jenkins - v333"
+
+echo "This is a very simple script for testing Jenkins - TRY"
