3b89b48f988ab5764438b3e7fb3f00314fbe8a39
turkanamammadova <60044927+turkanamammadova@users.noreply.github.com>
2021-01-30 08:36:47 -0600

    Update simple.sh
    this is last commit of mine

--- simple.sh
+++ simple.sh
@@ -5,3 +5,5 @@ echo "This is a very simple script for testing Jenkins - v2"
 echo "This is a very simple script for testing Jenkins - v333"
 
 echo "This is a very simple script for testing Jenkins - TRY"
+
+echo "This is a very simple script for testing Jenkins - TRY AGAIN"
